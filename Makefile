b44e52a90d19e8ec231cff5b83b9c9241e57d43d
Jesse Emmanuel Catalan <catalan@lnxsrv09.seas.ucla.edu>
2018-05-31 20:03:01 -0700

    Creating files

---
+++ Makefile
@@ -0,0 +1,3 @@
+# NAME: Ricardo Kuchimpos,Jesse Catalan
+# EMAIL: rkuchimpos@gmail.com,jessecatalan77@gmail.com
+# ID: 704827423,204785152
---
+++ lab3b.c
@@ -0,0 +1,7 @@
+/* NAME: Ricardo Kuchimpos,Jesse Catalan
+ * EMAIL: rkuchimpos@gmail.com,jessecatalan77@gmail.com
+ * ID: 704827423,204785152
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
