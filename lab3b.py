10b8ad77b1e54ddc58b1487c5dff7e986676fd04
rkuchimpos <rkuchimpos@live.com>
2018-05-31 21:56:01 -0700

    Create sublists for each summary category

--- lab3b.py
+++ lab3b.py
@@ -12,4 +12,18 @@ if __name__ == "__main__":
         sys.stderr.write("Invalid number of arguments!\nUsage: ./lab3b [csv]\n")
         exit(1)
 
+    filename = sys.argv[1]
+    f = open(filename, 'r')
+    fs_summary = f.readlines()
+    f.close()
 
+    superblock = filter(lambda line: line.startswith("SUPERBLOCK"), fs_summary)
+    indirect_blocks = filter(lambda line: line.startswith("INDIRECT"), fs_summary)
+    inodes = filter(lambda line: line.startswith("INODE"), fs_summary)
+    free_blocks = filter(lambda line: line.startswith("BFREE"), fs_summary)
+    free_inodes = filter(lambda line: line.startswith("IFREE"), fs_summary)
+    dirents = filter(lambda line: line.startswith("DIRENT"), fs_summary)
+    groups = filter(lambda line: line.startswith("GROUP"), fs_summary)
+
+
+    exit(0)
